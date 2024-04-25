6c199c6dc6ff825bc30bc3ab45d4ea762b4f053e
innomvula <65461268+innomvula@users.noreply.github.com>
2024-04-25 11:58:56 -0400

    created html and js files for rock paper scissors game and created key variables

---
+++ ropasc.js
@@ -0,0 +1,6 @@
+const bestOf = parseInt(prompt("Best of: "))
+const rps = ["rock", "paper", "scissors"]
+const winScore = Math.floor(bestOf/2) + 1
+let userScore = 0
+let cpuScore = 0
+let haveWinScore = false
